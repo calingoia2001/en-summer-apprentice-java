af2634d26db07484888457490d583602bb5b6b0c
Calin G <calingoia01@yahoo.com>
2023-07-19 15:53:00 +0300

    add Customer class in entity package

---
+++ src/main/java/com/example/Java/Ticket/Project/entities/Customer.java
@@ -0,0 +1,58 @@
+package com.example.Java.Ticket.Project.entities;
+
+import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Entity
+@Table(name = "CUSTOMER")
+public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "customer_sequence"
+    )
+    private Long customerID;
+    @NotNull
+    @Column ( name = "customerName" )
+    private String customerName;
+
+    @NotNull
+    @Column ( unique = true )
+    private String email;
+
+    public Customer() {
+    }
+
+    public Customer(Long customerID, String customerName, String email) {
+        this.customerID = customerID;
+        this.customerName = customerName;
+        this.email = email;
+    }
+
+    public Long getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(Long customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
