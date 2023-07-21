efb624029021fcab7eb28a41c13242b8ff8bae62
Calin G <calingoia01@yahoo.com>
2023-07-21 10:24:46 +0300

    add missing files

---
+++ src/main/java/com/example/Java/Ticket/Project/JavaTicketProjectApplication.java
@@ -0,0 +1,11 @@
+package com.example.Java.Ticket.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTicketProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTicketProjectApplication.class, args);
+	}
+}
